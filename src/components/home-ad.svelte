a06041844a89ed8166815a7f95f53d5266e50763
Nate <nate.hunzaker@gmail.com>
2020-02-22 13:17:47 -0800

    Ads!

--- src/components/home-ad.svelte
+++ src/components/home-ad.svelte
@@ -7,7 +7,8 @@
 <style>
   a {
     display: block;
-    max-width: 22em;
+    max-width: 384px;
+    width: 100%;
     margin: 2rem auto;
     border: 1px solid var(--color-fiber);
     box-shadow: 0 0 2px rgba(137, 149, 141, 0.51);
--- src/routes/_ads.js
+++ src/routes/_ads.js
@@ -1,8 +1,32 @@
 export const ADS = [
   {
-    slug: "puppies-mingle",
-    title: "Puppies Mingle",
     alt: "Puppies Mingle - An app for puppy singles",
-    src: "/ads/puppies-mingle.jpg"
+    slug: "puppies-mingle",
+    src: "/ads/puppies-mingle.jpg",
+    title: "Puppies Mingle"
+  },
+  {
+    alt: "Pup of Joe Coffee - Wake up and put the zoom in your zoomies.",
+    slug: "pup-of-joe",
+    src: "/ads/pup-of-joe.jpg",
+    title: "Pup of Joe"
+  },
+  {
+    alt: "Barkley and Wags - does your human owe you treats and walkies? Call the law offices of Barkley and Wags.",
+    slug: "barkley-and-wags",
+    src: "/ads/barkley-and-wags.jpg",
+    title: "Barkey and Wags"
+  },
+  {
+    alt: "Keep your kitchen floors clean with Scrapz Sharks",
+    slug: "scrapz-sharks",
+    src: "/ads/scraps-sharks.jpg",
+    title: "Scraps Sharks"
+  },
+  {
+    alt: "Guard puppy - protecs from murderers, out of place objects on the sidewalk, other doggies, and interactions with other humans in public.",
+    slug: "guard-puppy",
+    src: "/ads/guard-puppy.jpg",
+    title: "Guard Puppy"
   }
 ];
