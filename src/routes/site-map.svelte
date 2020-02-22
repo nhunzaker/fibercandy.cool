305c9969b7fd9faa1a9ad673ce21e45d3101a119
Nate <nate.hunzaker@gmail.com>
2020-02-22 15:01:55 -0800

    Add site map to generate ads to avoid 400s

---
+++ src/routes/site-map.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { RECIPES } from "./_recipes.js";
+  import { ADS } from "./_ads.js";
+</script>
+
+<svelte:head>
+  <title>Site Map</title>
+</svelte:head>
+
+<ul>
+  {#each RECIPES as recipe}
+    <li>
+      <a href="/recipes/{recipe.slug}">{recipe.title}</a>
+    </li>
+  {/each}
+
+  {#each ADS as ad}
+    <li>
+      <a href="/ads/{ad.slug}">{ad.title}</a>
+    </li>
+  {/each}
+</ul>
