034dde2d17e71fae96cbc6b912a7e8c760082a5e
Nate <nate.hunzaker@gmail.com>
2020-02-22 12:01:17 -0800

    Make ads links to pre-build

--- src/components/home-ad.svelte
+++ src/components/home-ad.svelte
@@ -5,25 +5,15 @@
 </script>
 
 <style>
-  form {
+  a {
+    display: block;
     max-width: 22em;
     margin: 2rem auto;
     border: 1px solid var(--color-fiber);
     box-shadow: 0 0 2px rgba(137, 149, 141, 0.51);
   }
-  button {
-    padding: 0;
-    border: 0;
-    background: 0;
-    display: block;
-  }
 </style>
 
-<form action="/ads/{ad.slug}">
-  <input type="hidden" value={ad.slug} />
-
-  <button>
-    <img src={ad.src} alt={ad.alt} />
-    <span class="visuallyhidden">View this ad</span>
-  </button>
-</form>
+<a href="/ads/{ad.slug}">
+  <img src={ad.src} alt={ad.alt} />
+</a>
